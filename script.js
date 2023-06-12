0791a389354428061dd5b721d50c311780624411
restunandita16 <136143487+restunandita16@users.noreply.github.com>
2023-06-12 14:35:54 +0700

    1 file modified

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const buttons = document.querySelectorAll("[data-carousel-button")
+const buttons = document.querySelectorAll('[data-carousel-button');
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
     const slides = button
-      .closest("[data-carousel]")
-      .querySelector('[data-slides]')
-   
-      const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
-    
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+      .closest('[data-carousel]')
+      .querySelector('[data-slides]');
+
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
 
 /*Customer Review Section*/
-
 var swiper = new Swiper('.review-slider', {
   slidesPerView: 1,
   spaceBetween: 10,
+  grabCursor: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
